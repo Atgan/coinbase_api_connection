59a92fac45743765c9f06c1e0a3645d8c90fb549
Metisdevs <63692234+Metisdevs@users.noreply.github.com>
2024-03-07 13:38:52 +0300

    removed keys. also deleted keys. simplified auth to coinbase by using their sdk.

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -1,59 +1,69 @@
 import json
-import hmac
-import hashlib
-import time
-import requests
-from requests.auth import AuthBase
-from coinbase import jwt_generator  # Import the JWT generator module
-
-# Set your API key and secret
-API_KEY = 'organizations/4bf9d615-1d1c-40c5-8896-56572884d7e9/apiKeys/9d40b8f4-c0ba-4869-8599-a3bd8ae8757a'
-API_SECRET = '-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIFl4TVXnYh/p06W+4+LW3UeAlA+JT0kA5VPXVkEbe+10oAoGCCqGSM49\nAwEHoUQDQgAElfyad3FJnEIejeGEecgJZx2cnpFehWuQjhDtIOqiKfLhrL0BF1fz\nINx4Z1bC6usPILL7qrg+1ER5X4/8Ui6YqQ==\n-----END EC PRIVATE KEY-----\n'
-
-# Create custom authentication for Coinbase API
-class CoinbaseWalletAuth(AuthBase):
-    def __init__(self, api_key, secret_key):
-        self.api_key = api_key
-        self.secret_key = secret_key
-
-    def __call__(self, request):
-        timestamp = str(int(time.time()))
-        message = timestamp + request.method + request.path_url + (request.body or '')
-        signature = hmac.new(self.secret_key.encode(), message.encode(), hashlib.sha256).hexdigest()
-
-        request.headers.update({
-            'CB-ACCESS-SIGN': signature,
-            'CB-ACCESS-TIMESTAMP': timestamp,
-            'CB-ACCESS-KEY': self.api_key,
-            'Content-Type': 'application/json'
-        })
-        return request
+from coinbase import jwt_generator  
+import uuid
+import http.client
 
 
-api_base_url = 'https://api.coinbase.com'
+
+api_key = 'organizations/4bf9d615-1d1c-40c5-8896-56572884d7e9/apiKeys/31e57904-4d28-4d17-b2ae-7480559725d5'
+api_secret = '-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEINlQRo2++kJamgT0LogsL4Nj4YHyqEYDYZtjC7RY7vMroAoGCCqGSM49\nAwEHoUQDQgAEFGUQU0yDBGNtwLmsF+8H5I0gbF+hWhXhQKolVtCwhnyf0c+sEv5c\nSTn18ck0Q20WvmEHjIUBTm361+aTawwtdA==\n-----END EC PRIVATE KEY-----\n'
+request_method = "POST"
+request_path = "/api/v3/brokerage/orders"
 
 
-auth = CoinbaseWalletAuth(API_KEY, API_SECRET)
 
-def generate_jwt_token(request_method, request_path):
-    expiration_time = getattr(generate_jwt_token, 'expiration_time', 0)
-    current_time = time.time()
-    if current_time >= expiration_time:
-        jwt_uri = jwt_generator.format_jwt_uri(request_method, request_path)
-        jwt_token = jwt_generator.build_rest_jwt(jwt_uri, API_KEY, API_SECRET)
-        generate_jwt_token.expiration_time = current_time + 120
-        generate_jwt_token.token = jwt_token
-    return generate_jwt_token.token
+def generate_jwt_token(request_path, request_method):
+    jwt_uri = jwt_generator.format_jwt_uri(request_method, request_path)
+    jwt_token = jwt_generator.build_rest_jwt(jwt_uri, api_key, api_secret)
+    return jwt_token
 
-try:
+def make_request():
     request_path = '/api/v3/brokerage/accounts'
+    request_method = 'GET'
+    jwt_token = generate_jwt_token()
+    headers = {
+        'Authorization': f'Bearer {jwt_token}'
+    }
+
+    conn = http.client.HTTPSConnection('api.coinbase.com')
+    conn.request(request_method, request_path, headers=headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data.decode("utf-8"))
+
+
+def buy_btc(amount_usd, amount_btc, unique_order_id):
+    request_path = '/api/v3/brokerage/orders'
+    request_method = 'POST'
+    jwt_token = generate_jwt_token(request_path, request_method)
+    headers = {
+        'Authorization': f'Bearer {jwt_token}'
+    }
+    payload = {
+        
+        'client_order_id': 'testm1',
+        'product_id': 'BTC-USD',
+        'side': 'BUY',
+        'order_configuration': {
+            'market_market_ioc': {
+                'quote_size': '5',  # Specify the amount of USD you want to spend
+            }
+        }
+    }
+    payload_str = json.dumps(payload)
+    conn = http.client.HTTPSConnection('api.coinbase.com')
+    conn.request(request_method, request_path, payload_str, headers=headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data.decode("utf-8"))
+
 
     
-    r = requests.get(api_base_url + request_path, headers={'Authorization': f'Bearer {generate_jwt_token("GET", request_path)}'})
-    r.raise_for_status()  
-    print(r.json()) 
-except requests.exceptions.HTTPError as err:
-    print("HTTP Error:", err)
-except json.decoder.JSONDecodeError:
-    print("Response does not contain valid JSON data.")
-    print("Status code:", r.status_code)
+ 
+
+if __name__ == "__main__":
+    unique_order_id = "testj" #provide this-you can pass this to get details on a trage etc
+    amount_btc=str('0.00007465')
+    amount_usd=str('5')
+    buy_btc(amount_btc,amount_usd, unique_order_id)
+   
